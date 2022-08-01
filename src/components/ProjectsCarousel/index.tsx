e4eeab1661c699f2799cb1df0a91599fccae7afb
hgalvao98 <henrique.galvao@hotmail.com>
2022-08-01 20:33:50 -0300

    added loading condition for projects carousel

--- src/components/ProjectsCarousel/index.tsx
+++ src/components/ProjectsCarousel/index.tsx
@@ -2,9 +2,10 @@ import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 import { ProjectData } from '../../types';
 import { CarouselMain } from './styles';
+import { ThreeDots } from 'react-loader-spinner';
 
 export const ProjectsCarousel = () => {
-  const [info, setInfo] = useState<ProjectData>([]);
+  const [info, setInfo] = useState<ProjectData>();
 
   const active = 'carousel-item active';
   const notActive = 'carousel-item';
@@ -26,86 +27,90 @@ export const ProjectsCarousel = () => {
 
   return (
     <CarouselMain>
-      <div
-        id="carouselExampleCaptions"
-        className="carousel slide"
-        data-bs-ride="false"
-      >
-        <div className="carousel-inner" role="listbox">
-          {info.map((proj, i) => {
-            return (
-              <div key={i} className={i === 0 ? active : notActive}>
-                <img
-                  src={proj.backgroundImg}
-                  className="d-block w-100"
-                  alt={proj.backgroundImg}
-                />
-                <div className="carousel-caption d-none d-md-block">
-                  <h5>
-                    {proj.title1}
-                    <span> {proj.title2}</span>
-                  </h5>
-                  <div className="carousel-links">
-                    <a target="_blank" rel="noreferrer" href={proj.website}>
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        width="70"
-                        height="70"
-                        fill="black"
-                        className="bi bi-link"
-                        viewBox="0 0 16 16"
-                      >
-                        <path d="M6.354 5.5H4a3 3 0 0 0 0 6h3a3 3 0 0 0 2.83-4H9c-.086 0-.17.01-.25.031A2 2 0 0 1 7 10.5H4a2 2 0 1 1 0-4h1.535c.218-.376.495-.714.82-1z" />
-                        <path d="M9 5.5a3 3 0 0 0-2.83 4h1.098A2 2 0 0 1 9 6.5h3a2 2 0 1 1 0 4h-1.535a4.02 4.02 0 0 1-.82 1H12a3 3 0 1 0 0-6H9z" />
-                      </svg>
-                    </a>
-                    <a target="_blank" rel="noreferrer" href={proj.github}>
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        width="70"
-                        height="70"
-                        fill="black"
-                        className="bi bi-github"
-                        viewBox="0 0 16 16"
-                      >
-                        <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.012 8.012 0 0 0 16 8c0-4.42-3.58-8-8-8z" />
-                      </svg>
-                    </a>
-                  </div>
-                  <div className="carousel-caption-text">
-                    <p>{proj.description}</p>
-                    <p>Technologies used: {proj.technologies}</p>
+      {info ? (
+        <div
+          id="carouselExampleCaptions"
+          className="carousel slide"
+          data-bs-ride="false"
+        >
+          <div className="carousel-inner" role="listbox">
+            {info.map((proj, i) => {
+              return (
+                <div key={i} className={i === 0 ? active : notActive}>
+                  <img
+                    src={proj.backgroundImg}
+                    className="d-block w-100"
+                    alt={proj.backgroundImg}
+                  />
+                  <div className="carousel-caption d-none d-md-block">
+                    <h5>
+                      {proj.title1}
+                      <span> {proj.title2}</span>
+                    </h5>
+                    <div className="carousel-links">
+                      <a target="_blank" rel="noreferrer" href={proj.website}>
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          width="70"
+                          height="70"
+                          fill="black"
+                          className="bi bi-link"
+                          viewBox="0 0 16 16"
+                        >
+                          <path d="M6.354 5.5H4a3 3 0 0 0 0 6h3a3 3 0 0 0 2.83-4H9c-.086 0-.17.01-.25.031A2 2 0 0 1 7 10.5H4a2 2 0 1 1 0-4h1.535c.218-.376.495-.714.82-1z" />
+                          <path d="M9 5.5a3 3 0 0 0-2.83 4h1.098A2 2 0 0 1 9 6.5h3a2 2 0 1 1 0 4h-1.535a4.02 4.02 0 0 1-.82 1H12a3 3 0 1 0 0-6H9z" />
+                        </svg>
+                      </a>
+                      <a target="_blank" rel="noreferrer" href={proj.github}>
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          width="70"
+                          height="70"
+                          fill="black"
+                          className="bi bi-github"
+                          viewBox="0 0 16 16"
+                        >
+                          <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.012 8.012 0 0 0 16 8c0-4.42-3.58-8-8-8z" />
+                        </svg>
+                      </a>
+                    </div>
+                    <div className="carousel-caption-text">
+                      <p>{proj.description}</p>
+                      <p>Technologies used: {proj.technologies}</p>
+                    </div>
                   </div>
                 </div>
-              </div>
-            );
-          })}
+              );
+            })}
+          </div>
+          <button
+            className="carousel-control-prev"
+            type="button"
+            data-bs-target="#carouselExampleCaptions"
+            data-bs-slide="prev"
+          >
+            <span
+              className="carousel-control-prev-icon"
+              aria-hidden="true"
+            ></span>
+            <span className="visually-hidden">Previous</span>
+          </button>
+          <button
+            className="carousel-control-next"
+            type="button"
+            data-bs-target="#carouselExampleCaptions"
+            data-bs-slide="next"
+          >
+            <span
+              className="carousel-control-next-icon"
+              aria-hidden="true"
+            ></span>
+            <span className="visually-hidden">Next</span>
+          </button>
         </div>
-        <button
-          className="carousel-control-prev"
-          type="button"
-          data-bs-target="#carouselExampleCaptions"
-          data-bs-slide="prev"
-        >
-          <span
-            className="carousel-control-prev-icon"
-            aria-hidden="true"
-          ></span>
-          <span className="visually-hidden">Previous</span>
-        </button>
-        <button
-          className="carousel-control-next"
-          type="button"
-          data-bs-target="#carouselExampleCaptions"
-          data-bs-slide="next"
-        >
-          <span
-            className="carousel-control-next-icon"
-            aria-hidden="true"
-          ></span>
-          <span className="visually-hidden">Next</span>
-        </button>
-      </div>
+      ) : (
+        <ThreeDots color="#6c63ffb2" height={80} width={80} />
+      )}
     </CarouselMain>
   );
 };
